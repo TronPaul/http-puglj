1ee6aedc0f9e56feef300331dec61e31eb86d1cc
Mark McGuire <mark.b.mcg@gmail.com>
2014-11-20 23:20:35 -0600

    kill unused methods

--- src/http_puglj/core.clj
+++ src/http_puglj/core.clj
@@ -23,19 +23,6 @@
 (defn dev-mode? []
   true)
 
-(defn resolve-uri
-  [context uri]
-  (let [context (if (instance? URI context) context (URI. context))]
-    (.resolve context uri)))
-
-(defn context-uri
-  "Resolves a [uri] against the :context URI (if found) in the provided
-  Ring request. (Only useful in conjunction with compojure.core/context.)"
-  [{:keys [context]} uri]
-  (if-let [base (and context (str context "/"))]
-    (str (resolve-uri base uri))
-    uri))
-
 (defn template-base-variables [req]
   {:id (friend/identity req)
    :login-action "/login"
